7752151d0161bf421a5db447fcf21578d51cfba3
UladKasach <nappa300@gmail.com>
2018-01-19 13:16:42 -0500

    complete processing data into model readable format

--- feature_learning/_data_manager/__init__.py
+++ feature_learning/_data_manager/__init__.py
@@ -12,12 +12,14 @@ import operator;
 import _utilities as utils;
 import hashlib;
 import json;
+import collections;
+import numpy as np;
 
 logger = logging.getLogger(__name__)
 
-score_ranges_by_essay_set = {
+score_ranges_by_essay_set = { ## this defines the range of domain_1 scores for each essay set
 	1: (2,12),
-	2: (1,6), # note, this only utilizes the first score domain.
+	2: (1,6),
 	3: (0,3),
 	4: (0,3),
 	5: (0,4),
@@ -25,6 +27,53 @@ score_ranges_by_essay_set = {
 	7: (0,30),
 	8: (0,60)
 }
+
+def convert_scores_to(to_what, orig_scores, essay_sets):
+    def to_raw(orig_score, scale, base):
+		return orig_score * scale + base ## orig_score * scale - base
+    def to_norm(orig_score, scale, base):
+        return (orig_score - base) / float(scale);
+    def convert_this_score(orig_score, essay_set):
+        assert essay_set in score_ranges_by_essay_set; ## ensure essay_set is valid
+    	low, high = score_ranges_by_essay_set[essay_set];
+        scale = high - low;
+        base = low;
+        assert to_what in ["raw", "norm"]; ## ensure to_what is valid
+        if(to_what == "raw"):
+            score = to_raw(orig_score, scale, base);
+            assert score >= low and score <= high; ## ensure that final score is valid logically
+        elif(to_what == "norm"):
+            score = to_norm(orig_score, scale, base);
+            assert score >= 0 and score <= 1;
+        return score;
+
+    ## normalize the input
+    assert type(orig_scores) in {int, float, np.ndarray, list}; ## ensure that orig_score is either a float or a list of floats
+    assert type(essay_sets) in {int, np.ndarray, list}; ## ensure that essay set is either an int or a list of ints
+    originally_single_value = False;
+    if(type(essay_sets) == int): ## if its a single element, then cast both into a list to interpret in a general way
+        orig_scores = [orig_scores];
+        essay_sets = [essay_sets];
+        originally_single_value = True;
+    orig_scores = np.array(orig_scores); ## ensure orig_scores are  np.array
+    essay_sets = np.array(essay_sets); ## ensure essay_set are np.array
+    assert orig_scores.shape[0] == essay_sets.shape[0] ## ensure scores and essay_set ids are same size
+    data_matrix = np.column_stack([orig_scores, essay_sets]); ## convert into "tuples" on each row of a n by 2 matrix
+
+    ## generate list of converted scores
+    scores = [];
+    for row in data_matrix:
+        orig_score, essay_set = row;
+        score = convert_this_score(orig_score, essay_set);
+        logger.debug(score);
+        scores.append(score);
+
+    ## return data
+    if(originally_single_value): ## ensure that if only one score was provided originally (not a list) to output a socre, not a list
+        scores = scores[0];
+    return scores;
+
+
 
 def retreive_data_from_fold_part(fold_base_path, part_type):
     input_file = fold_base_path+"."+part_type+".csv";
@@ -49,7 +98,6 @@ def tokenize(string):
             tokens.pop(index)
     return tokens
 def build_vocab(header, dataset, options_dict):
-
     total_words, unique_words = 0, 0;
     word_freqs = dict();
 
@@ -77,6 +125,92 @@ def build_vocab(header, dataset, options_dict):
     statistics = (total_words, unique_words);
 
     return vocab, statistics;
+def generate_vocab(raw_data, fold_base_path, bool_overwrite_pickle): ## either builds or retreives from cache
+    ## 1.a. build vocab based on training data (use nltk)
+
+    text_options_dict = dict({ ## TODO - implement to_lower and vocab size functionality. Will need to reflect to_lower in cleanse_raw_data tokenization
+        "to_lower": False,
+        "vocab_size" : False,
+    });
+
+    logger.info('    building vocabulary from training data...');
+    options_encoding = hashlib.sha1(json.dumps(text_options_dict, sort_keys=True)).hexdigest()  ## used to encode selected options into filename: ensures that if options are changed we use a cache for that option set
+    cleaned_file_name = fold_base_path.replace("/", "__");
+
+    create_vocab = False; ## evaluate whether or not to create the vocab or just load it from cache
+    if(bool_overwrite_pickle):
+        create_vocab = True;
+    else:
+        try:
+            vocab, statistics = utils.cache.retreive_from_cache(cleaned_file_name + "-" + options_encoding);
+        except IOError:
+            logger.info('        vocab does not exist in cache...');
+            create_vocab = True;
+
+    if(create_vocab): ## create the vocab if needed based on previous logic
+        logger.info('        creating vocab...')
+        vocab, statistics = build_vocab(raw_data["header"], raw_data["train"], text_options_dict);
+        logger.info('        caching vocab....');
+        utils.cache.save_to_cache(cleaned_file_name + "-" + options_encoding, [vocab, statistics]);
+
+    logger.info('        %i total words, %i unique words' % statistics);
+    return vocab;
+
+def cleanse_this_essay(essay_data, header, vocab):
+    ## initialize cleansed data w/ base data
+    essay_id = int(essay_data[header.index("essay_id")]);
+    essay_set = int(essay_data[header.index("essay_set")]);
+    cleansed_data = dict({
+        "essay_id" : essay_id,
+        "essay_set": essay_set,
+    })
+
+    ## 2.a tokenize text (based on vocab);
+    stats = dict({
+        "total" : 0,
+        "num" : 0,
+        "unk" : 0,
+    })
+    raw_text = essay_data[header.index("essay")];
+    tokenized_text_pre_vocab = tokenize(raw_text);
+    tokenized_indicies = [];
+    for token in tokenized_text_pre_vocab:
+        if token.replace('.','',1).isdigit():## if the token is a number, replace it with index of <num>
+            tokenized_indicies.append(vocab["<num>"]);
+            stats["num"] += 1;
+        elif token in vocab: ## else if its in vocabulary, replace with index (put after number check since we dont remove number tokens in vocabulary building as it would require us to check if its numeric twise)
+            tokenized_indicies.append(vocab[token]);
+        else:
+            tokenized_indicies.append(vocab["<unk>"]);
+            stats["unk"] += 1;
+        stats["total"] += 1;
+    cleansed_data["essay_tokens"] = tokenized_indicies;
+
+    ## 2.b normalize scores
+    raw_score = float(essay_data[header.index("domain1_score")]); ## use domain1_score as score for essay
+    normalized_score = convert_scores_to("norm", raw_score, essay_set);
+    cleansed_data["raw_score"] = raw_score;
+    cleansed_data["norm_score"] = normalized_score;
+
+    return cleansed_data, stats;
+
+def cleanse_raw_data(dataset, header, vocab, type):
+    logger.info("    cleansing raw " + type + " data (tokenization and score normalization)... ")
+    ## cleanse each essay data
+    stats = dict({
+        "total" : 1,
+        "num" : 1,
+        "unk" : 1,
+    })
+    cleansed_data = [];
+    for essay_data in dataset:
+        this_cleansed_data, these_stats = cleanse_this_essay(essay_data, header, vocab);
+        cleansed_data.append(this_cleansed_data);
+        stats = dict(collections.Counter(stats)+collections.Counter(these_stats)); ## sum stats and these stats by key,  https://stackoverflow.com/a/30950164/3068233
+
+    logger.info('        <num> hit rate: %d (%.2f%%), <unk> hit rate: %d (%.2f%%), of %d' % (stats["num"], 100*stats["num"]/float(stats["total"]), stats["unk"], 100*stats["unk"]/float(stats["total"]), stats["total"]))
+
+    return cleansed_data;
 
 def get_data_for_fold(fold_base_path, bool_overwrite_pickle): ## bool_overwrite_pickle enables the automatic cache to be overwritten
     print("retreive dataset from fold base name " + fold_base_path);
@@ -96,60 +230,19 @@ def get_data_for_fold(fold_base_path, bool_overwrite_pickle): ## bool_overwrite_
     ## process data:
     ##  1. text processing:
     ##    a. build vocabulary based on training data
-    ##    b. tokenize text
-    ##  2. score processing
-    ##    a. normalize scores
-    ##  3. strip irrelevant fields
+    ##  2. build cleaned data
+    ##    a. tokenize text (based on vocab)
+    ##    b. normalize scores
 
-    ## 1.a. build vocab based on training data (use nltk)
-    logger.info('    building vocabulary from training data...');
-    options_dict = dict({
-        "to_lower": False,
-        "vocab_size" : False,
-    });
-    options_encoding = hashlib.sha1(json.dumps(options_dict, sort_keys=True)).hexdigest()  ## used to encode selected options into filename: ensures that if options are changed we use a cache for that option set
-    cleaned_file_name = fold_base_path.replace("/", "__");
+    ## 1. text processing
+    vocab = generate_vocab(raw_data, fold_base_path, bool_overwrite_pickle);
 
-    create_vocab = False; ## evaluate whether or not to create the vocab or just load it from cache
-    if(bool_overwrite_pickle):
-        create_vocab = True;
-    else:
-        try:
-            vocab, statistics = utils.cache.retreive_from_cache(cleaned_file_name + "-" + options_encoding);
-        except IOError:
-            logger.info('        vocab does not exist in cache...');
-            create_vocab = True;
+    ## 2. build cleaned data
+    cleaned_data = dict({
+        "train" : cleanse_raw_data(raw_data["train"], raw_data["header"], vocab, "train"),
+        "tune" : cleanse_raw_data(raw_data["tune"], raw_data["header"], vocab, "tune"),
+        "test" : cleanse_raw_data(raw_data["test"], raw_data["header"], vocab, "test"),
+        "header" : ["essay_id", "essay_set", "essay_tokens", "raw_score", "normalized_score"],
+    })
 
-    if(create_vocab): ## create the vocab if needed based on previous logic
-        logger.info('        creating vocab...')
-        vocab, statistics = build_vocab(raw_data["header"], raw_data["train"], options_dict);
-        logger.info('        caching vocab....');
-        utils.cache.save_to_cache(cleaned_file_name + "-" + options_encoding, [vocab, statistics]);
-
-    logger.info('        %i total words, %i unique words' % statistics);
-    print ("here i am!");
-    print(vocab["the"]);
-
-
-    '''
-    def get_data(paths, prompt_id, vocab_size, maxlen, tokenize_text=True, to_lower=True, sort_by_len=False, vocab_path=None, score_index=6):
-    	train_path, dev_path, test_path = paths[0], paths[1], paths[2]
-
-    	if not vocab_path:
-    		vocab = create_vocab(train_path, prompt_id, maxlen, vocab_size, tokenize_text, to_lower)
-    		if len(vocab) < vocab_size:
-    			logger.warning('The vocabualry includes only %i words (less than %i)' % (len(vocab), vocab_size))
-    		else:
-    			assert vocab_size == 0 or len(vocab) == vocab_size
-    	else:
-    		vocab = load_vocab(vocab_path)
-    		if len(vocab) != vocab_size:
-    			logger.warning('The vocabualry includes %i words which is different from given: %i' % (len(vocab), vocab_size))
-    	logger.info('  Vocab size: %i' % (len(vocab)))
-
-    	train_x, train_y, train_prompts, train_maxlen = read_dataset(train_path, prompt_id, maxlen, vocab, tokenize_text, to_lower)
-    	dev_x, dev_y, dev_prompts, dev_maxlen = read_dataset(dev_path, prompt_id, 0, vocab, tokenize_text, to_lower)
-    	test_x, test_y, test_prompts, test_maxlen = read_dataset(test_path, prompt_id, 0, vocab, tokenize_text, to_lower)
-
-    	overal_maxlen = max(train_maxlen, dev_maxlen, test_maxlen)
-    '''
+    return cleaned_data;
